f3f060f1a2a91a58b21a40beb0305b53adccecfd
Quinn Lapish <quinn.lapish99@gmail.com>
2019-09-14 18:27:49 -0400

    all code

--- server.js
+++ server.js
@@ -1,28 +1,14 @@
 var express = require("express");
-
 var PORT = process.env.PORT || 3306;
-
 var app = express();
-
 app.use(express.static("public"));
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
-var exphbs = require("index-handlebars");
-
+var exphbs = require("index.handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
-
-
-var routes = require("./controllers/catsController.js");
-
+var routes = require("./controllers/burgers_controllers");
 app.use(routes);
-
-
 app.listen(PORT, function() {
-
-  console.log("Server listening on: http://localhost:" + PORT);
+ console.log("Server listening on: http://localhost:" + PORT);
 });
